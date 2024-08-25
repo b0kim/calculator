e6b5e6276e08f836436c1737c8093baabaee3543
b0kim <71298559+b0kim@users.noreply.github.com>
2024-08-25 07:14:40 -0400

    Added js code to display numeric input

--- script.js
+++ script.js
@@ -1,14 +1,76 @@
 const prevExpression = document.querySelector('.previous.expression');
 const currExpression = document.querySelector('.current.expression');
 
-
+let prevResult;
 let first;
 let second;
 let operator;
 
+const numberButtons = document.querySelectorAll('.number-btn');
+numberButtons.forEach( button => {
+    button.addEventListener('click', () => {
+        const digitChoice = button.dataset.choice; 
+        parseDigitInput(digitChoice);
+    })
+});
+
+function parseDigitInput(digit) {
+    /*
+    IF second
+        APPEND digit to end of second
+    ELSE IF operator
+        SET second = digit
+    ELSE IF first
+        APPEND digit to end of first
+    ELSE
+        SET = first (if there is a previous result, wipe it out 
+                and start new expression)
+    */
+    // if none aka display cleared
+    if (first) {
+        first += digit;
+        currExpression.textContent = first;
+    }
+    else {
+        first = digit;
+        currExpression.textContent = first;
+    }
+}
 
 
-
+/*
+IF button == digit:
+    IF second
+        APPEND digit to end of second
+    ELSE IF operator
+        SET second = digit
+    ELSE IF first
+        APPEND digit to end of first
+    ELSE
+        SET = first (if there is a previous result, wipe it out and start new expression)
+ELSE IF button == operator
+    IF there is already first and second number:
+        CALCULATE the result (as if we pressed equals)
+        STORE the result in first
+        SET operator
+        SET second to null
+    ELSE if there is a previous result
+        SET first = previous result
+        SET operator
+    ELSE if there is a first number
+        SET operator
+    ELSE 
+        do nothing
+    ENDIF
+ELSE IF button == equals
+    IF first and second and operator
+        CALCULATE the result
+        STORE result in prevResult
+        set first, second, and operator to null
+    ELSE
+        do nothing (?)
+    ENDIF
+*/
 
 
 function operate(operator, first, second) {
